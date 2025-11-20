23ecde5e38054f017f4ca0ea6240ef62e6e5a589
blazgeev <igor.blazgeevskyy@valuetek.com.ua>
2025-11-20 18:42:09 +0100

    feat: AI generated TODO list

---
+++ homework-1/README.md
@@ -0,0 +1,10 @@
+```
+python3 -m venv venv
+
+source venv/bin/activate
+
+python3 -m pip install django
+
+python3 -c "import django; print(django.get_version())"
+
+```
---
+++ homework-1/todo/forms.py
@@ -0,0 +1,10 @@
+from django import forms
+from .models import Todo
+
+class TodoForm(forms.ModelForm):
+    class Meta:
+        model = Todo
+        fields = ['title', 'description', 'due_date']
+        widgets = {
+            'due_date': forms.DateTimeInput(attrs={'type': 'datetime-local'}),
+        }
