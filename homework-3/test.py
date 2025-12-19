fe965dd290bae4699821c7087b0a254983786aea
blazgeev <igor.blazgeevskyy@valuetek.com.ua>
2025-12-19 18:01:09 +0100

    chore: remove ignored files and add .gitignore for homework-3

--- homework-3/test.py
+++
@@ -1,8 +0,0 @@
-from main import _fetch_web_content
-
-url = "https://github.com/alexeygrigorev/minsearch"
-try:
-    content = _fetch_web_content(url)
-    print(f"Content length: {len(content)}")
-except Exception as e:
-    print(f"Error: {e}")
--- homework-3/test_search_tool.py
+++
@@ -1,4 +0,0 @@
-from main import _search_documentation
-
-query = "demo"
-print(_search_documentation(query))
